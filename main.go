6ac3027a34b0b0b335919eb81441d1d9ae16f156
Nitin Misra <nitin.misra23@gmail.com>
2017-09-05 20:56:37 +0530

    Capture OS signal in the main thread

--- main.go
+++ main.go
@@ -12,65 +12,58 @@ import (
 )
 
 var (
-	ServiceUrl     = flag.String("u", "", "HTTP endpoint URL {string} (Required)")
-	PingInterval   = flag.Int("i", 5, "Ping Interval {integer} (Seconds) (Optional) (Default 5 seconds)")
-	RequestTimeout = flag.Int("t", 5, "Request Timeout {integer} (Seconds) (Optional) (Default 2 seconds)")
+	serviceUrl     = flag.String("u", "", "HTTP endpoint URL {string} (Required)")
+	pingInterval   = flag.Int("i", 5, "Ping Interval {integer} (Seconds) (Optional) (Default 5 seconds)")
+	requestTimeout = flag.Int("t", 2, "Request Timeout {integer} (Seconds) (Optional) (Default 2 seconds)")
+	quitChan       = make(chan os.Signal, 1)
 )
-
-var quitChan chan bool
 
 func init() {
 	flag.Parse()
 
-	if *ServiceUrl == "" {
+	if *serviceUrl == "" {
 		log.Printf("Missing required flags\n")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	go SignalHandler(make(chan os.Signal, 1))
-	log.Printf("Start pinging %s every %v seconds\n", *ServiceUrl, *PingInterval)
+	AddSignalHandler()
+	log.Printf("Start pinging %s every %v seconds\n", *serviceUrl, *pingInterval)
 }
 
 func main() {
-	pinger := p.NewPinger(*ServiceUrl, *RequestTimeout)
-
-	ticker := time.NewTicker(time.Duration(*PingInterval) * time.Second)
-	quitChan = make(chan bool)
+	pinger := p.NewPinger(*serviceUrl, *requestTimeout)
+	ticker := time.NewTicker(time.Duration(*pingInterval) * time.Second)
 
 	for {
 		select {
 		case <-ticker.C:
 			err := pinger.Ping()
 			if err != nil {
-				log.Println("Error while pinging URL", *ServiceUrl, err)
+				log.Println("Error while pinging URL", *serviceUrl, err)
 			} else {
-				log.Println("Ping successful", *ServiceUrl)
+				log.Println("Ping successful", *serviceUrl)
 			}
-		case <-quitChan:
+		case s := <-quitChan:
 			ticker.Stop()
-			return
+
+			switch s {
+			case syscall.SIGABRT:
+				log.Println("Aborting...")
+				os.Exit(0)
+			case os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT:
+				log.Println("Shutting Down...")
+				os.Exit(1)
+			}
 		}
 	}
 }
 
-func closeTicker() {
-	quitChan <- true
-	close(quitChan)
-}
-
-func SignalHandler(c chan os.Signal) {
-	signal.Notify(c, os.Interrupt, syscall.SIGABRT, syscall.SIGTERM, syscall.SIGQUIT)
-	for s := <-c; ; s = <-c {
-		switch s {
-		case syscall.SIGABRT:
-			log.Println("Aborting...")
-			closeTicker()
-			os.Exit(0)
-		case os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT:
-			log.Println("Shutting Down...")
-			closeTicker()
-			os.Exit(1)
-		}
-	}
+func AddSignalHandler() {
+	signal.Notify(
+		quitChan,
+		os.Interrupt,
+		syscall.SIGABRT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
 }
